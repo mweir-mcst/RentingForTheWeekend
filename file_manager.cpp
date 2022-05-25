5c5ffc9c01ce761c1d08ab57d4069576898dc393
Matthew Weir <matthew.weir@mcvts.org>
2022-05-25 10:05:42 -0400

    feat: file management

--- file_manager.cpp
+++ file_manager.cpp
@@ -20,10 +20,29 @@ vector<User*> load_from_file() {
         getline(fin, currentValue);
         if (currentValue.empty()) {
             currentUser++;
+            users.push_back(new User(currentUserInfo[0], currentUserInfo[1], stoi(currentUserInfo[2]), currentUserInfo[3], stoi(currentUserInfo[4]), stoi(currentUserInfo[5]), currentUserInfo[6] == "true", currentUserInfo[7] == "true", currentUserInfo[8] == "true"));
+            users.clear();
         } else {
             currentUserInfo.push_back(currentValue);
         }
     }
 
+    fin.close();
     return users;
+}
+
+void save_to_file(vector<User*>& users) {
+    ofstream fout("users.txt");
+
+    for (User* user : users) fout << user->cardHolder << endl
+        << user->address << endl
+        << user->memberCount << endl
+        << user->emergencyContact << endl
+        << user->rentalDays << endl
+        << user->vehicle << endl
+        << user->roadsideAssistance << endl
+        << user->gasFillCoverage << endl
+        << user->driverFaultCoverage << endl << endl;
+
+    fout.close();
 }
